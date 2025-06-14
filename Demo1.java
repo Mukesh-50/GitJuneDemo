b092be8b12c2395fc4861b6db914f49fa588b31a
Mukesh Otwani <mukeshotwani.50@gmail.com>
2025-06-14 23:02:44 +0530

    Method 1,2,3

--- Demo1.java
+++ Demo1.java
@@ -3,7 +3,17 @@ public class Demo1()
 
     public void method1()
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World 1");
+    }
+
+    public void method2()
+    {
+        System.out.println("Hello World 2");
+    }
+
+    public void method3()
+    {
+        System.out.println("Hello World 3");
     }
 
 
