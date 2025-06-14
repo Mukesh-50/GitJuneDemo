a48dc4b8f09ea7fdc234a165fd2649ab017edefe
Mukesh Otwani <mukeshotwani.50@gmail.com>
2025-06-14 23:09:56 +0530

    added changes

--- Demo1.java
+++ Demo1.java
@@ -3,7 +3,28 @@ public class Demo1()
 
     public void method1()
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World 1");
+    }
+
+    public void method2()
+    {
+        System.out.println("Hello World 2");
+    }
+
+    public void method3()
+    {
+        System.out.println("Hello World 3");
+    }
+
+    public void method4()
+    {
+        System.out.println("Hello World 4");
+    }
+
+
+    public void method5()
+    {
+        System.out.println("Hello World 5");
     }
 
 
